4af4a9f2d091efa98d05fc2445aa9c247454a823
Nwankwo <pascalernest.nnagozie@gmail.com>
2022-09-09 16:08:07 -0700

    put.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main. Print in multilingual
+ * 
+ * Return:Always 0 (done)
+ */
+int main()
+{
+	puts(''Programming is like building a multilingual puzzle'');
+	return (0)
+}
