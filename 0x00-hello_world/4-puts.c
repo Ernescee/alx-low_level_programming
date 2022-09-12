f7bca5424f9128075085b1647d166e25c93b264f
Nwankwo <pascalernest.nnagozie@gmail.com>
2022-09-12 14:20:51 -0700

    solving difficult task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,25 +1,11 @@
 #include <stdio.h>
-
-
-
 /**
-
-* main - Printing
-
+* main - entering point
 *
-
-* Return: Always 0 (Successful)
-
+* Return: 0 after (Successful printing)
 */
-
 int main(void)
-
 {
-
-puts("\"Programming is like building a multilingual
-
-puzzle");
-
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
-
 }
