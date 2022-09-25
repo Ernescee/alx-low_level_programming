fb57ef157e551cc057a8caf4ccefeebe009499b1
Nwankwo <pascalernest.nnagozie@gmail.com>
2022-09-25 05:12:45 -0700

    ammending task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * string_toupper -> converting any string to upper case * @x: string param
+ * string_toupper -> converting any string to upper case
+ * @x: string param
  * Return: string
- */ 
+ */
 char *string_toupper(char *x)
 {
 	int i = 0;
